21f54e5c8cd61ee4dec20c27397279d7e24c7783
jandortiz <jefferson.garciaor@gmail.com>
2024-12-28 18:16:35 +0100

    style: organiza el código para cargar información a S3 en forma de función.

--- python/s3_connection/s3_upload.py
+++ python/s3_connection/s3_upload.py
@@ -1,20 +1,26 @@
-import boto3
+
 import os
+import boto3
 
-bucket_name = 'tfm-data-pdf'
+import streamlit as st
+def cargar_archivos_s3():
 
-# TODO: Encontrar la forma de subir archivos que previamente no se han cargado
-# a S3.
+    bucket_name = st.secrets['s3_bucket']['BUCKET_NAME']
 
-s3_client = boto3.client('s3')
-# s3_client.create_bucket(
-#     Bucket=bucket_name,
-#     CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
+    # TODO: Encontrar la forma de subir archivos que previamente no se han
+    # cargado a S3.
 
+    s3_client = boto3.client('s3')
+    # s3_client.create_bucket(
+    #     Bucket=bucket_name,
+    #     CreateBucketConfiguration={'LocationConstraint': 'eu-central-1'})
 
-data_path = os.path.join('data')
-for root, _, files in os.walk(data_path):
-    for file in files:
-        file_path = os.path.join(root, file)
-        s3_client.upload_file(file_path, bucket_name, f"{file_path}")
-        print(f'archivo {file_path} cargado.')
+    data_path = os.path.join('data')
+    for root, _, files in os.walk(data_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            s3_client.upload_file(file_path, bucket_name, f"{file_path}")
+            print(f'archivo {file_path} cargado.')
+
+if __name__=="__main__":
+    cargar_archivos_s3()
