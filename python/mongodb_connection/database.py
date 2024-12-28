144e31132e1908a42839106920d01e61821ee4ac
jandortiz <jefferson.garciaor@gmail.com>
2024-12-28 17:57:14 +0100

    docs: crea la documentación de la función cargar_datos_a_mongodb.

--- python/mongodb_connection/database.py
+++ python/mongodb_connection/database.py
@@ -6,35 +6,44 @@ from pymongo import MongoClient
 from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.mongodb import MongoDBAtlasVectorSearch
-from llama_index.core import SimpleDirectoryReader, StorageContext, VectorStoreIndex
+from llama_index.core import (
+    SimpleDirectoryReader,
+    StorageContext, VectorStoreIndex)
 
 from s3fs import S3FileSystem
 
-from huggingface_hub import login
-
 import streamlit as st
 
-from dotenv import load_dotenv
-load_dotenv()
+from huggingface_hub import login
 
-Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en")
+embedding_name = st.secrets['HF_embeddings']['HF_EMBEDDING']
+Settings.embed_model = HuggingFaceEmbedding(model_name=embedding_name)
 
 
 @st.cache_resource
 def cargar_datos_a_mongodb(uri: str):
+    """Lee, procesa, indexa y carga la información relacionada con los pdf's a
+    MongoDB.
+
+    Args:
+        uri: link que contiene la información para conectarse a MongoDB.
+
+    Returns:
+        Índice vectorial de cada pdf procesado.
+    """
 
     # Se crea el cliente y conecta  con la colección.
     cliente_mongodb = MongoClient(uri, timeoutMS=60000, socketTimeoutMS=60000)
     bucket_name = 'tfm-data-pdf/data'
     
     try:
-        login(token=os.getenv('HF_TOKEN'))
+        login(token=st.secrets['huggingface_conn']['HF_TOKEN'])
         s3_fs = S3FileSystem()
-        print('')
-        print(s3_fs.ls(bucket_name))
 
         loader = SimpleDirectoryReader(
-            input_dir=bucket_name, fs=s3_fs, recursive=True, filename_as_id=True)
+            input_dir=bucket_name, fs=s3_fs, recursive=True,
+            filename_as_id=True)
+        
         datos = loader.load_data()
         
         atlas_vector_store = MongoDBAtlasVectorSearch(
@@ -44,10 +53,11 @@ def cargar_datos_a_mongodb(uri: str):
             vector_index_name='vector_index'
             )
         
-        storage_context = StorageContext.from_defaults(vector_store=atlas_vector_store)
+        storage_context = StorageContext.from_defaults(
+            vector_store=atlas_vector_store)
 
-        
-        index = VectorStoreIndex.from_documents(documents=datos, storage_context=storage_context)
+        index = VectorStoreIndex.from_documents(
+            documents=datos, storage_context=storage_context)
         return index
         
 
