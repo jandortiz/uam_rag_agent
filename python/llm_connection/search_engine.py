ecf92f83e6f3ac627c89394cc100583a6764ff5b
jandortiz <jefferson.garciaor@gmail.com>
2024-12-30 10:40:06 +0100

    style: quita la importación de la clase ChatMessage, la cuál no se usa.

--- python/llm_connection/search_engine.py
+++ python/llm_connection/search_engine.py
@@ -8,7 +8,6 @@ from pymongo import MongoClient
 from huggingface_hub import login
 
 from groq import Groq
-from llama_index.core.llms import ChatMessage
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 
